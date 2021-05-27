1a0a41d0eefe80c86193aad440444a5372b50aaf
Megan Howley <meglhowley@gmail.com>
2021-05-27 17:02:07 -0400

    playing with backend

--- models/restaurant.js
+++ models/restaurant.js
@@ -5,8 +5,7 @@ const Restaurant = new Schema({
   name: { type: String, required: true },
   distance: { type: String, required: true },
   category: { type: String, required: true },
-  rating: { type: String, required: true },
-  menuItem1: { type: Object, required: true },
-  menuItem2: { type: Object, required: true },
-  menuItem3: { type: Object, required: true }
+  rating: { type: String, required: true }
 })
+
+module.exports = mongoose.model('restaurants', Restaurant)
