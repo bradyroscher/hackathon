57aed947323aa818570cbb7b1aa97638a89b98b4
Megan Howley <meglhowley@gmail.com>
2021-05-27 19:47:17 -0400

    api and component map rendering on screen

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const routes = require('./routes')
 const db = require('./db')
+const cors = require('cors')
+const logger = require('morgan')
 const bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 3001
 
 const app = express()
+app.use(cors())
 
 app.use(bodyParser.json())
 app.use('/api', routes)
