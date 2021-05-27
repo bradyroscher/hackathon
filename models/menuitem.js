6c75a26fe2d30cd33ab67100e8429b3d1ba9e84b
Brady Roscher <bradyroscher@yahoo.com>
2021-05-27 16:04:57 -0400

    built out server??"

--- models/menuitem.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const MenuItem = new Schema({
-  name: { type: String, required: true },
-  price: { type: String, required: true }
-})
--- models/restaurant.js
+++ models/restaurant.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
-const MenuItemSchema = require('./menuitem')
 const Schema = mongoose.Schema
 
 const Restaurant = new Schema({
   name: { type: String, required: true },
   distance: { type: String, required: true },
   category: { type: String, required: true },
-  rating: { type: String, required: true }
-  // menuItemsArray:
+  rating: { type: String, required: true },
+  menuItem1: { type: Object, required: true },
+  menuItem2: { type: Object, required: true },
+  menuItem3: { type: Object, required: true }
 })
